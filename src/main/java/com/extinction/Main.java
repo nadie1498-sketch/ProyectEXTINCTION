39eb3d39ab7d7451722805deb76d894d7ea6a0e1
nadie1498-sketch <nadie1498@gmail.com>
2026-08-02 20:46:54 -0500

    Update Main.java

--- src/main/java/com/extinction/Main.java
+++ src/main/java/com/extinction/Main.java
@@ -13,13 +13,49 @@ import java.util.List;
 
 public final class Main extends JavaPlugin {
 
+    public static ItemStack extintSword;
+    public static ItemStack extintChestplate;
+
     @Override
     public void onEnable() {
         getLogger().info("¡ProyectEXTINCTION activado correctamente!");
+        createExtinctItems();
     }
 
     @Override
     public void onDisable() {
         getLogger().info("ProyectEXTINCTION desactivado.");
+    }
+
+    private void createExtinctItems() {
+        // --- Espada Extinta ---
+        extintSword = new ItemStack(Material.NETHERITE_SWORD);
+        ItemMeta swordMeta = extintSword.getItemMeta();
+        if (swordMeta != null) {
+            swordMeta.setDisplayName(ChatColor.DARK_RED + "" + ChatColor.BOLD + "Espada Extinta");
+            List<String> lore = new ArrayList<>();
+            lore.add(ChatColor.GRAY + "Una espada legendaria de tiempos olvidados.");
+            lore.add(ChatColor.DARK_PURPLE + "Poder abrumador.");
+            swordMeta.setLore(lore);
+            swordMeta.addEnchant(Enchantment.DAMAGE_ALL, 10, true);
+            swordMeta.addEnchant(Enchantment.FIRE_ASPECT, 5, true);
+            swordMeta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+            extintSword.setItemMeta(swordMeta);
+        }
+
+        // --- Armadura Extinta ---
+        extintChestplate = new ItemStack(Material.NETHERITE_CHESTPLATE);
+        ItemMeta chestMeta = extintChestplate.getItemMeta();
+        if (chestMeta != null) {
+            chestMeta.setDisplayName(ChatColor.GOLD + "" + ChatColor.BOLD + "Armadura Extinta");
+            List<String> lore = new ArrayList<>();
+            lore.add(ChatColor.GRAY + "Forjada con restos de un mundo destruido.");
+            lore.add(ChatColor.DARK_PURPLE + "Resistencia extrema.");
+            chestMeta.setLore(lore);
+            chestMeta.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 10, true);
+            chestMeta.addEnchant(Enchantment.DURABILITY, 3, true);
+            chestMeta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+            extintChestplate.setItemMeta(chestMeta);
+        }
     }
 }
