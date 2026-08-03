05d2c707e56bf7d6e383b36e89b1908e1eb126d8
nadie1498-sketch <nadie1498@gmail.com>
2026-08-02 21:01:04 -0500

    Update Main.java

--- src/main/java/com/extinction/Main.java
+++ src/main/java/com/extinction/Main.java
@@ -2,8 +2,11 @@ package com.extinction;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -11,15 +14,14 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.util.ArrayList;
 import java.util.List;
 
-public final class Main extends JavaPlugin {
-
-    public static ItemStack extintSword;
-    public static ItemStack extintChestplate;
+public final class Main extends JavaPlugin implements CommandExecutor {
 
     @Override
     public void onEnable() {
         getLogger().info("¡ProyectEXTINCTION activado correctamente!");
-        createExtinctItems();
+        if (getCommand("extinction") != null) {
+            getCommand("extinction").setExecutor(this);
+        }
     }
 
     @Override
@@ -27,35 +29,50 @@ public final class Main extends JavaPlugin {
         getLogger().info("ProyectEXTINCTION desactivado.");
     }
 
-    private void createExtinctItems() {
-        // --- Espada Extinta ---
-        extintSword = new ItemStack(Material.NETHERITE_SWORD);
-        ItemMeta swordMeta = extintSword.getItemMeta();
-        if (swordMeta != null) {
-            swordMeta.setDisplayName(ChatColor.DARK_RED + "" + ChatColor.BOLD + "Espada Extinta");
-            List<String> lore = new ArrayList<>();
-            lore.add(ChatColor.GRAY + "Una espada legendaria de tiempos olvidados.");
-            lore.add(ChatColor.DARK_PURPLE + "Poder abrumador.");
-            swordMeta.setLore(lore);
-            swordMeta.addEnchant(Enchantment.DAMAGE_ALL, 10, true);
-            swordMeta.addEnchant(Enchantment.FIRE_ASPECT, 5, true);
-            swordMeta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
-            extintSword.setItemMeta(swordMeta);
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("Este comando solo puede ser usado por jugadores.");
+            return true;
         }
 
-        // --- Armadura Extinta ---
-        extintChestplate = new ItemStack(Material.NETHERITE_CHESTPLATE);
-        ItemMeta chestMeta = extintChestplate.getItemMeta();
-        if (chestMeta != null) {
-            chestMeta.setDisplayName(ChatColor.GOLD + "" + ChatColor.BOLD + "Armadura Extinta");
-            List<String> lore = new ArrayList<>();
-            lore.add(ChatColor.GRAY + "Forjada con restos de un mundo destruido.");
-            lore.add(ChatColor.DARK_PURPLE + "Resistencia extrema.");
-            chestMeta.setLore(lore);
-            chestMeta.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 10, true);
-            chestMeta.addEnchant(Enchantment.DURABILITY, 3, true);
-            chestMeta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
-            extintChestplate.setItemMeta(chestMeta);
+        Player player = (Player) sender;
+
+        if (command.getName().equalsIgnoreCase("extinction")) {
+            // --- Crear Espada Extinta ---
+            ItemStack extintSword = new ItemStack(Material.NETHERITE_SWORD);
+            ItemMeta swordMeta = extintSword.getItemMeta();
+            if (swordMeta != null) {
+                swordMeta.setDisplayName(ChatColor.DARK_RED + "" + ChatColor.BOLD + "Espada Extinta");
+                List<String> lore = new ArrayList<>();
+                lore.add(ChatColor.GRAY + "Una espada legendaria de tiempos olvidados.");
+                swordMeta.setLore(lore);
+                swordMeta.addEnchant(Enchantment.DAMAGE_ALL, 10, true);
+                swordMeta.addEnchant(Enchantment.FIRE_ASPECT, 5, true);
+                swordMeta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+                extintSword.setItemMeta(swordMeta);
+            }
+
+            // --- Crear Armadura Extinta ---
+            ItemStack extintChestplate = new ItemStack(Material.NETHERITE_CHESTPLATE);
+            ItemMeta chestMeta = extintChestplate.getItemMeta();
+            if (chestMeta != null) {
+                chestMeta.setDisplayName(ChatColor.GOLD + "" + ChatColor.BOLD + "Armadura Extinta");
+                List<String> lore = new ArrayList<>();
+                lore.add(ChatColor.GRAY + "Forjada con restos de un mundo destruido.");
+                chestMeta.setLore(lore);
+                chestMeta.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 10, true);
+                chestMeta.addEnchant(Enchantment.DURABILITY, 3, true);
+                chestMeta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+                extintChestplate.setItemMeta(chestMeta);
+            }
+
+            // Entregar al jugador
+            player.getInventory().addItem(extintSword);
+            player.getInventory().addItem(extintChestplate);
+            player.sendMessage(ChatColor.GREEN + "¡Has recibido los ítems de ProyectEXTINCTION!");
+            return true;
         }
+        return false;
     }
 }
